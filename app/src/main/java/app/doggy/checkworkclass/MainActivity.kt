c75495eacb548d8f60b632839915024279e370da
Kaito-Dogi <kendobu0405@gmail.com>
2021-12-05 01:55:45 +0900

    add: UserクラスをSharedPrefに保存して表示する

--- app/src/main/java/app/doggy/checkworkclass/MainActivity.kt
+++ app/src/main/java/app/doggy/checkworkclass/MainActivity.kt
@@ -5,6 +5,9 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import app.doggy.checkworkclass.databinding.ActivityMainBinding
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,9 +22,15 @@ class MainActivity : AppCompatActivity() {
         val sharedPref: SharedPreferences = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
 
         // 保存した内容を表示する．
-        binding.nameText.text = sharedPref.getString(NAME, "名前")
-        binding.courseText.text = sharedPref.getString(COURSE, "コース")
-        binding.commentText.text = sharedPref.getString(COMMENT, "チェックワーク最高！")
+//        binding.nameText.text = sharedPref.getString(NAME, "名前")
+//        binding.courseText.text = sharedPref.getString(COURSE, "コース")
+//        binding.commentText.text = sharedPref.getString(COMMENT, "チェックワーク最高！")
+        val defaultUser = User("名前", "コース", "チェックワーク最高")
+        val userString = sharedPref.getString(USER, Json.encodeToString(defaultUser)) as String
+        val user = Json.decodeFromString<User>(userString)
+        binding.nameText.text = user.name
+        binding.courseText.text = user.course
+        binding.commentText.text = user.comment
 
         // save_buttonを押したときの処理
         binding.saveButton.setOnClickListener {
@@ -29,9 +38,15 @@ class MainActivity : AppCompatActivity() {
             val editor = sharedPref.edit()
 
             // Step1：EditTextに入力した値をEditorに渡す．
-            editor.putString(NAME, binding.nameEditText.text.toString())
-            editor.putString(COURSE, binding.courseEditText.text.toString())
-            editor.putString(COMMENT, binding.commentEditText.text.toString())
+//            editor.putString(NAME, binding.nameEditText.text.toString())
+//            editor.putString(COURSE, binding.courseEditText.text.toString())
+//            editor.putString(COMMENT, binding.commentEditText.text.toString())
+            val user = User(
+                binding.nameEditText.text.toString(),
+                binding.courseEditText.text.toString(),
+                binding.commentEditText.text.toString(),
+            )
+            editor.putString(USER, Json.encodeToString(user))
 
             // 渡した値を保存する．
             editor.apply()
