a38a0fcc13f84b89bf50b9342ce5424209abea91
Kaito-Dogi <kendobu0405@gmail.com>
2021-12-04 23:24:27 +0900

    add: viewBindingの有効化

--- app/src/main/java/app/doggy/checkworkclass/MainActivity.kt
+++ app/src/main/java/app/doggy/checkworkclass/MainActivity.kt
@@ -2,10 +2,15 @@ package app.doggy.checkworkclass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.doggy.checkworkclass.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // バインディングクラスの変数
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
     }
 }
